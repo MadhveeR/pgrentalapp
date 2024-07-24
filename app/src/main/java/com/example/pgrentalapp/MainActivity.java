4d28ff10726ac4366f676d8f5634c2083efdadcd
Janvi Rathod <rmadhvi942@gmail.com>
2024-07-24 13:54:42 -0700

    Login Form Java

---
+++ app/src/main/java/com/example/pgrentalapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.pgrentalapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signin;
+    EditText email,password;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signin = findViewById(R.id.main_signin);
+        email =findViewById(R.id.main_email);
+        password =findViewById(R.id.main_password);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else {
+                    System.out.println("Loging Successfully");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Loging Successfully", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
